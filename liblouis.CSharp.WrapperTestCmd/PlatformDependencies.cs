59f85e74fbdeb739ba2094b5bfd208d42c8b249c
JensJensenPublic <65678793+JensJensenPublic@users.noreply.github.com>
2024-02-24 16:29:15 +0100

    Implement PlatformDependencies for using only Console, no real Logfile

--- liblouis.CSharp.WrapperTestCmd/PlatformDependencies.cs
+++ liblouis.CSharp.WrapperTestCmd/PlatformDependencies.cs
@@ -28,43 +28,20 @@ namespace LibLouisWrapperTestCmd
 
         static internal void Log(string s)
         {
-            string cm = GetCallingMethod(1);
+            string cm = Utilities.GetCallingMethod(1);
             Console.WriteLine(string.Format("{0} {1}",cm,s));  
         }
 
         static internal void OnWrapperLog(string s)
         {
-            string cm = GetCallingMethod(2);
+            string cm = Utilities.GetCallingMethod(2);
             Console.WriteLine(string.Format("{0} {1}", cm, s));
         }
         static internal void OnLibLouisLog(string s)
         {
-            string cm = GetCallingMethod(3);
+            string cm = Utilities.GetCallingMethod(3);
             Console.WriteLine(string.Format("{0} {1}", cm, s));
         }
-
-        static private string GetCallingMethod(int extraLevels)
-        {
-            return GetCallingMethodInternalImplementation(2 + extraLevels); //  because we use this extra level for calling GetCallingMethod(int levels) !
-        }
-
-
-        static private string GetCallingMethodInternalImplementation(int levels)
-        {
-            try
-            {
-                StackTrace stackTrace = new StackTrace();
-                MethodBase methodBase = stackTrace.GetFrame(levels + 1).GetMethod(); // "levels+1" in order to compensate for calling "GetCallingMethod()"
-                Type type = methodBase.ReflectedType;
-                return string.Format("{0}.{1}", type.Name, methodBase.Name);
-            }
-            catch (Exception e)
-            {
-                return string.Format("Logger.GetCallingMethod threw an exception: {0}", e.ToString());
-            }
-        }
-
-
 
     }
 }
